90a64b2b3c1d507a2b770e7bd982abc330ae592e
lotus <lian82717pro@gmail.com>
2024-01-10 15:24:18 +0800

    feat(header): 增加登入前後狀態 - 用memberInfo當作假的使用者狀態 - 根據不同的登入狀態來決定選單及頭像的顯示

--- src/components/Header/CtrlBar.tsx
+++ src/components/Header/CtrlBar.tsx
@@ -4,7 +4,13 @@ import NavLink from "./NavLink";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMagnifyingGlass, faUser, faAngleRight } from "@fortawesome/free-solid-svg-icons";
 
+const memberInfo = {
+  status: false,
+  userImg: 'https://fakeimg.pl/300x300/'
+};
+
 interface AccountItemProps {
+  className?: string,
   children: React.ReactNode;
   onItemClicked: () => void;
   path: string;
@@ -36,12 +42,24 @@ function CtrlBar() {
             placeholder="搜尋"
           />
           <div className="y-centered right-5 w-6 h-6 rounded-full bg-gray-400 cursor-pointer md:right-1">
-            <FontAwesomeIcon className="centered text-sm text-white " icon={faMagnifyingGlass} />
+              <FontAwesomeIcon className="centered text-sm text-white " icon={faMagnifyingGlass} />
           </div>
         </div>
       </nav>
-      <div className={`account-btn w-8 h-8 rounded-full bg-gray-400 relative cursor-pointer shrink-0`} onClick={() => { toggleAccount(); setIsMobileMenu(false); }}>
-        <FontAwesomeIcon className="centered text-lg text-white" icon={faUser} />
+      <div className={`account-btn w-8 h-8 rounded-full overflow-hidden bg-gray-400 relative cursor-pointer shrink-0`} onClick={() => { toggleAccount(); setIsMobileMenu(false); }}>
+
+        {
+          memberInfo.status ?
+            (
+              <div>
+                <img src={memberInfo.userImg} alt="" />
+              </div>
+            )
+            :
+            (
+              <FontAwesomeIcon className="centered text-lg text-white" icon={faUser} />
+            )
+        }
       </div>
       <div className={`${isMobileMenu ? '-active' : ''} mobile-toggle-btn md:hidden relative w-8 h-6 cursor-pointer`} onClick={() => { toggleMobileMenu(); setIsAccountVisible(false); }}>
         <span className='block absolute top-0 bg-white w-full h-1 rounded-full'></span>
@@ -51,16 +69,37 @@ function CtrlBar() {
       {isAccountVisible && (
         <div className='fixed top-0 left-0 w-full h-screen account-layer' onClick={(e) => {
           const accountContainer = document.querySelector('.account-container');
-          if(!accountContainer?.contains(e.target as Node)) {
+          if (!accountContainer?.contains(e.target as Node)) {
             setIsAccountVisible(false);
           }
         }}>
           <div className="account-container box-shadow absolute top-16 right-8 border-box pt-1 pb-1 w-40 rounded-md bg-white">
-            <ul>
-              <AccountItem path="/LogIn" onItemClicked={() => { setIsAccountVisible(false); }}>
-                登入
-              </AccountItem>
-            </ul>
+            {
+              memberInfo.status ?
+                (
+                  <ul>
+                    <AccountItem path="/" onItemClicked={() => { setIsAccountVisible(false); }}>
+                      <p>會員資料</p>
+                    </AccountItem>
+                    <AccountItem path="/" onItemClicked={() => { setIsAccountVisible(false); }}>
+                      <p>訊息中心</p>
+                    </AccountItem>
+                    <AccountItem path="/" onItemClicked={() => { setIsAccountVisible(false); }}>
+                      <p>追蹤清單</p>
+                    </AccountItem>
+                    <AccountItem className='text-red-500' path="/" onItemClicked={() => { setIsAccountVisible(false); }}>
+                      <p>登出</p>
+                    </AccountItem>
+                  </ul>
+                ) :
+                (
+                  <ul>
+                    <AccountItem path="/LogIn" onItemClicked={() => { setIsAccountVisible(false); }}>
+                      <p>登入</p>
+                    </AccountItem>
+                  </ul>
+                )
+            }
           </div>
         </div>
       )}
@@ -68,13 +107,14 @@ function CtrlBar() {
   )
 }
 
-function AccountItem({ children, onItemClicked, path }: AccountItemProps) {
+function AccountItem({ className, children, onItemClicked, path }: AccountItemProps) {
   return (
-    <li className='hover:bg-gray-100 cursor-pointer flex items-center justify-between' onClick={onItemClicked}>
-      <Link to={path} className='w-full pt-2 pb-2 pl-3 pr-3 relative'><p>{children}</p></Link>
+    <li className={`${className} relative hover:bg-gray-100 cursor-pointer flex items-center justify-between`} onClick={onItemClicked}>
+      <Link to={path} className='w-full pt-2 pb-2 pl-3 pr-3 relative'>{children}</Link>
       <FontAwesomeIcon className='absolute top-1/2 -translate-y-1/2 right-3 pointer-events-none' icon={faAngleRight} />
     </li>
   );
 }
+
 
 export default CtrlBar;
